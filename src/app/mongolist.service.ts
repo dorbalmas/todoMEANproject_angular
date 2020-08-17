14ef94f1f9a314c4aca0bd9c9deba8c8f7c97cc7
dor balmas <dorbal31@gmail.com>
2020-08-17 17:04:59 +0300

    17082020

--- src/app/mongolist.service.ts
+++ src/app/mongolist.service.ts
@@ -4,26 +4,26 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class MongolistService {
-  url = 'http://localhost:3000/tasks';
+  url = 'https://tibatodosproject.herokuapp.com/tasks';
   constructor(private http: HttpClient) {}
   getData() {
     return this.http.get(this.url);
   }
   getSearchedData(_searchProp) {
-    this.url = `http://localhost:3000/tasks/SearchTodo/?q=${_searchProp}`;
+    this.url = `https://tibatodosproject.herokuapp.com/tasks/SearchTodo/?q=${_searchProp}`;
     return this.http.get(this.url);
   }
   addTodo(_obj) {
     console.warn(_obj);
-    this.url = 'http://localhost:3000/tasks/AddTodo';
+    this.url = 'https://tibatodosproject.herokuapp.com/tasks/AddTodo';
     return this.http.post(this.url, _obj);
   }
   removeTodo(_obj) {
-    this.url = 'http://localhost:3000/tasks/RemoveTodo';
+    this.url = 'https://tibatodosproject.herokuapp.com/tasks/RemoveTodo';
     return this.http.post(this.url, _obj);
   }
   editTodoObj(_obj) {
-    this.url = 'http://localhost:3000/tasks/EditTodo';
+    this.url = 'https://tibatodosproject.herokuapp.com/tasks/EditTodo';
     return this.http.post(this.url, _obj);
   }
 }
